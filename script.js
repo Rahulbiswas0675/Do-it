9f8b34e4298f5e1fbac51817eaac71c9da392919
Rahul Biswas <100189999+Rahulbiswas0675@users.noreply.github.com>
2022-10-25 14:15:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function onAddEventHandler(event){
+    event.preventDefault();
+    let input = document.querySelector(".todo-input").value;
+    let globalElement = document.querySelector(".todo-list");
+    let createElementDiv = document.createElement("div"); //<div></div>
+    let createElementList = document.createElement("li"); //<li></li>
+    let createNodeList = document.createTextNode(input); // todolist
+    createElementList.classList.add("todo-item"); // <li class="todo-item"></li>
+    createElementList.appendChild(createNodeList); //<li class="todo-item">togoList</li>
+    createElementDiv.appendChild(createElementList);//<div><li class="todo-item">togoList</li></div>
+    let createElementAddButton = document.createElement("button");
+    let createNodeAddButton = document.createTextNode("Completed");
+    createElementAddButton.classList.add("complete-btn");
+    createElementAddButton.appendChild(createNodeAddButton);
+    createElementDiv.appendChild(createElementAddButton);
+    createElementAddButton.addEventListener('click',onCompleteEventListener);
+    let createElementRemoveButton = document.createElement("button");
+    let createNodeRemoveButton = document.createTextNode("Delete");
+    createElementRemoveButton.classList.add("trash-btn");
+    createElementRemoveButton.appendChild(createNodeRemoveButton);
+    createElementDiv.appendChild(createElementRemoveButton);
+    createElementRemoveButton.addEventListener('click',onDeleteHandler);
+    globalElement.appendChild(createElementDiv);//<ul><div><li class="todo-item">togoList</li></div></ul>
+}
+function onCompleteEventListener(event){
+    event.target.parentElement.firstChild.classList.toggle("complete");
+}
+function onDeleteHandler(event){
+    event.target.parentElement.remove();
+}
